1bde6432149aca01ecf86482a027278485981056
Darya Tsimashenkava (@besovadevka) <timaph@mail.ru>
2020-09-22 12:20:39 +0300

    feat: add checks for what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (arguments.length === 0) {
+    return 'Unable to determine the time of year!';
+  } else if (typeof date !== "object") {
+    throw new Error('Nope.');
+  } else {
+
+  }
 };
