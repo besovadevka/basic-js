c2c2ef83734d14d7418ad953444aeac0661e8531
Darya Tsimashenkava (@besovadevka) <timaph@mail.ru>
2020-09-25 15:30:04 +0300

    feat: try to done transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,41 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (Array.isArray(arr)) {
+    
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === "--discard-next") {
+        if (arr.indexOf("--discard-next") + 1  < arr.length) {
+          arr.splice(arr.indexOf("--discard-next"), 2);
+        } else {
+          arr.splice(-1, 1);
+        }
+      } else if (arr[i] === "--discard-prev") {
+        if (arr.indexOf("--discard-prev") - 1  > 0) {
+          arr.splice((arr.indexOf("--discard-prev") - 1), 2);
+        } else {
+          arr.splice(arr.indexOf("--discard-prev"), 1);
+        }
+      } else if (arr[i] === "--double-next") {
+        if (arr.indexOf("--double-next") + 1  < arr.length) {
+          arr[arr.indexOf("--double-next")] = arr[arr.indexOf("--double-next") + 1];
+        } else {
+          arr.splice(arr.indexOf("--double-next"), 1);
+        }
+      } else if (arr[i] === "--double-prev") {
+        if (arr.indexOf("--double-prev") - 1  > 0) {
+          arr.splice(arr.indexOf("--double-prev"), 1, arr[arr.indexOf("--double-prev") - 1]);
+          
+        } else  {
+          arr.splice(0, 1);
+        }
+      }
+      
+    }
+    return arr;
+  
+
+  } else {
+    throw Error("It is not... an... array(");
+  }
 };
